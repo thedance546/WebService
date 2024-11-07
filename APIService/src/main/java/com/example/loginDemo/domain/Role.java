8d484d98d2594094d87dbdba694aa5a096cd1c4a
hhh546 <tjdus3858@gmail.com>
2024-11-07 11:33:20 +0900

    delete apiservice

--- APIService/src/main/java/com/example/loginDemo/domain/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.loginDemo.domain;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- APIService/src/main/java/com/example/loginDemo/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.example.loginDemo.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
