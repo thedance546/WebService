12a2edd1b82f5efbfbc3b8a3628fd2348bbedcff
hhh546 <tjdus3858@gmail.com>
2024-11-14 17:23:41 +0900

    .

--- APIService/src/main/java/com/example/loginDemo/chat/ChatRequest.java
+++ APIService/src/main/java/com/example/loginDemo/chat/ChatRequest.java
@@ -2,7 +2,6 @@ package com.example.loginDemo.chat;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 // 사용자의 요청 메시지를 담는 DTO.
--- APIService/src/main/java/com/example/loginDemo/chat/ChatbotController.java
+++ APIService/src/main/java/com/example/loginDemo/chat/ChatbotController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/chat")
 @CrossOrigin(origins = "http://localhost:3000")
