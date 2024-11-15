e52da8c125c2d6d7d766f7f52ef93e7c98668f38
LAPTOP-5ICSOOU4\user <sunahj329@gmail.com>
2024-11-15 09:40:45 +0900

    yolo json

--- yolo/test.py
+++ yolo/test.py
@@ -13,13 +13,16 @@ with open(image_path, 'rb') as f:
 if response.status_code == 200:
     detections = response.json().get('detections', [])
     
-    # JSON 파일로 저장
-    with open("detection_results.json", "w", encoding="utf-8") as json_file:
-        json.dump({"detections": detections}, json_file, ensure_ascii=False, indent=4)
-
     # 객체 이름을 추출하여 개수를 계산
     item_counts = Counter([detection['name'] for detection in detections])
     
+    # 클래스 이름과 개수만 포함된 JSON 형식 생성
+    simplified_detections = [{"name": item, "count": count} for item, count in item_counts.items()]
+    
+    # JSON 파일로 저장
+    with open("detection_results_simplified.json", "w", encoding="utf-8") as json_file:
+        json.dump(simplified_detections, json_file, ensure_ascii=False, indent=4)
+
     # 요약된 결과 출력
     for item, count in item_counts.items():
         print(f"{item}: {count}개")
