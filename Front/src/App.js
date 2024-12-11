4730649174e4635eb450f47d3ef569fc431b98be
realstone <ting6247@nate.com>
2024-12-11 10:38:23 +0900

    notification test page

--- Front/src/App.js
+++ Front/src/App.js
@@ -7,8 +7,8 @@ import RegisterForm from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 
 import MyIngredients from './pages/MyIngredients';
-import ChatBot from './pages/ChatBot';
 import Notification from './pages/Notification';
+import ChatBot from './pages/ChatBot';
 import Settings from './pages/Settings';
 
 import { AdminProvider } from './contexts/AdminContext';
@@ -29,8 +29,8 @@ function App() {
 
         {/* 기능 탭 경로 */}
         <Route path="/my-ingredients" element={<MyIngredients />} />
-        <Route path="/chatbot" element={<ChatBot />} />
         <Route path="/notifications" element={<Notification />} />
+        <Route path="/chatbot" element={<ChatBot />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
       <AdminProvider>
--- Front/src/constants/navItems.js
+++ Front/src/constants/navItems.js
@@ -1,8 +1,8 @@
 // src/constants/navItems.js
 const navItems = [
     { path: '/my-ingredients', label: '나의 식재료' },
-    { path: '/chatbot', label: '챗봇' },
     { path: '/notifications', label: '알림' },
+    { path: '/chatbot', label: '챗봇' },
     { path: '/settings', label: '설정' },
   ];
   
