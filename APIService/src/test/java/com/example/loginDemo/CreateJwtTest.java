19164ea3915caab83970405d016dd452ad1c507f
hhh546 <tjdus3858@gmail.com>
2024-12-11 17:46:11 +0900

    3306

--- APIService/src/test/java/com/example/loginDemo/CreateJwtTest.java
+++ APIService/src/test/java/com/example/loginDemo/CreateJwtTest.java
@@ -3,10 +3,11 @@ package com.example.loginDemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@Profile("test")
 @SpringBootTest
 public class CreateJwtTest {
 
