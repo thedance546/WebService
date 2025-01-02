caaf28a34946e7a195f21af95140dcd3da65b99b
hhh546 <tjdus3858@gmail.com>
2025-01-02 11:51:37 +0900

    ocr matchedItems

--- APIService/src/main/java/com/example/loginDemo/controller/ItemController.java
+++ APIService/src/main/java/com/example/loginDemo/controller/ItemController.java
@@ -15,6 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/items")
--- APIService/src/main/java/com/example/loginDemo/repository/ItemRepository.java
+++ APIService/src/main/java/com/example/loginDemo/repository/ItemRepository.java
@@ -3,6 +3,8 @@ package com.example.loginDemo.repository;
 import com.example.loginDemo.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
     boolean existsByItemName(String itemName);
     void deleteByCategoryId(Long categoryId);
