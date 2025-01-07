4431fc77875a25a77457d0145ee7cbedb41bbb81
hhh546 <tjdus3858@gmail.com>
2025-01-07 15:26:35 +0900

    yolo 이미지

--- APIService/src/main/java/com/example/loginDemo/yolo/YoloController.java
+++ APIService/src/main/java/com/example/loginDemo/yolo/YoloController.java
@@ -1,8 +1,10 @@
 package com.example.loginDemo.yolo;
 
+import com.example.loginDemo.dto.MultipartFileRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -13,6 +15,7 @@ import java.util.*;
 @RequiredArgsConstructor
 public class YoloController {
     private final YoloService yoloService;
+    private final String Ingredient_URL = "http://yolo-container:5000/object-detection/object_detection";
 
     //ingredient
     @PostMapping("/items/detection")
@@ -22,6 +25,39 @@ public class YoloController {
             return ResponseEntity.ok(detectionResults);
         } catch (IOException e) {
             return ResponseEntity.status(500).body(null); // 내부 서버 오류
+        }
+    }
+
+    //ingredient-image
+    @PostMapping("/process")
+    public ResponseEntity<byte[]> processIngredientImage(@RequestParam("image") MultipartFile image) throws IOException {
+        // Flask 서버에 보낼 이미지 준비
+        byte[] imageBytes = image.getBytes();
+
+        // RestTemplate을 사용하여 Flask 서버로 이미지 전송
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 헤더 설정 (이미지 파일을 보내는 경우)
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        // 이미지 파일을 포함한 요청 본문 설정
+        MultipartFileRequest request = new MultipartFileRequest(imageBytes);
+
+        // Flask 서버에 이미지 전송
+        ResponseEntity<byte[]> response = restTemplate.exchange(
+                Ingredient_URL + "/object-detection/object_detection",
+                HttpMethod.POST,
+                request,
+                byte[].class
+        );
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(response.getBody());
+        } else {
+            return ResponseEntity.status(response.getStatusCode()).body(null);
         }
     }
 
