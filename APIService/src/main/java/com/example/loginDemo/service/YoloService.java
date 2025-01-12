64cdb0c4f9e8c1be6ea8c665668371d6fabda442
realstone <ting6247@nate.com>
2025-01-13 06:36:05 +0900

    .

--- APIService/src/main/java/com/example/loginDemo/service/YoloService.java
+++ APIService/src/main/java/com/example/loginDemo/service/YoloService.java
@@ -24,7 +24,7 @@ public class YoloService {
     private final RestTemplate restTemplate = new RestTemplate();
 //    private final String Ingredient_URL = "http://yolo-container-:5000/object-detection/object_detection";
 //    private final String Receipt_URL = "http://receipt-container-:5001/ocr-detection";
-    private final String Ingredient_URL = "http://yolo-container:5000/object-detection/object_detection";
+    private final String Ingredient_URL = "http://yolo-container:5000/object-detection/object_detection/image";
     private final String Receipt_URL = "http://receipt-container:5001/ocr-detection";
 
     public DetectionResponse detectAndReturn(MultipartFile imageFile) throws IOException {
