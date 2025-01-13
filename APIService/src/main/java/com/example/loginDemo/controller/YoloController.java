8e6dd4957d4416f040723a796b90bdb864617c9b
hhh546 <tjdus3858@gmail.com>
2025-01-13 10:29:26 +0900

    spring container version

--- APIService/src/main/java/com/example/loginDemo/controller/YoloController.java
+++ APIService/src/main/java/com/example/loginDemo/controller/YoloController.java
@@ -14,6 +14,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:8080")
 @RequiredArgsConstructor
 public class YoloController {
     private final YoloService yoloService;
