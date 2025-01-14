47e04e1f0065d5a087de5d6f798e2e7d2142370b
hhh546 <tjdus3858@gmail.com>
2025-01-14 09:59:03 +0900

    spring container version

--- APIService/src/main/java/com/example/loginDemo/service/ChatService.java
+++ APIService/src/main/java/com/example/loginDemo/service/ChatService.java
@@ -3,7 +3,6 @@ package com.example.loginDemo.service;
 
 import com.example.loginDemo.dto.ChatMessageDTO;
 import com.example.loginDemo.dto.RecipeResponse;
-import com.example.loginDemo.service.ChatBotService;
 import com.example.loginDemo.domain.*;
 import com.example.loginDemo.service.MessageService;
 import lombok.RequiredArgsConstructor;
