fbdffd37a3d8ea503746c4003d50c6f5f3638947
realstone <ting6247@nate.com>
2025-01-15 14:38:40 +0900

    홈페이지 애니메이션 효과

--- Front/src/pages/HomePage.tsx
+++ Front/src/pages/HomePage.tsx
@@ -1,12 +1,25 @@
 // src/pages/HomePage.tsx
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GlobalBackground from '../components/templates/GlobalBackground';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/atoms/Button';
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    // 컴포넌트가 마운트된 후 fadeIn 상태를 true로 설정
+    const timer = setTimeout(() => setFadeIn(true), 500);
+    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+  }, []);
+
+  const buttonStyle = {
+    transition: 'opacity 3s ease-in-out, transform 3s ease-in-out', // 3초 동안 천천히 나타남
+    opacity: fadeIn ? 1 : 0, // 투명도 제어
+    transform: fadeIn ? 'translateY(0)' : 'translateY(20px)', // 아래에서 위로 이동
+  };
 
   return (
     <GlobalBackground>
@@ -14,14 +27,14 @@ const Home: React.FC = () => {
         <Button
           onClick={() => navigate('/Register')}
           className="btn-lg w-100"
-          style={{ backgroundColor: '#DFF4E0', color: 'black' }}
+          style={{ ...buttonStyle, backgroundColor: '#DFF4E0', color: 'black' }}
         >
           회원가입
         </Button>
         <Button
           onClick={() => navigate('/Login')}
           className="btn-lg w-100"
-          style={{ backgroundColor: 'var(--primary-color)', color: 'black' }}
+          style={{ ...buttonStyle, backgroundColor: 'var(--primary-color)', color: 'black' }}
         >
           로그인
         </Button>
