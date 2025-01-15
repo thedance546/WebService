5aec0b94b81da6b3134e243538ea8984a85afd78
hhh546 <tjdus3858@gmail.com>
2025-01-15 15:05:30 +0900

    고기

--- APIService/src/main/java/com/example/loginDemo/service/YoloService.java
+++ APIService/src/main/java/com/example/loginDemo/service/YoloService.java
@@ -65,8 +65,19 @@ public class YoloService {
         // 아이템 매칭
         Set<String> matchedItemsSet = matchItems(items);
 
+        // 소, 닭, 돼지를 소고기, 닭고기, 돼지고기로 변경
+        Map<String, String> replacementMap = Map.of(
+                "소", "소고기",
+                "닭", "닭고기",
+                "돼지", "돼지고기"
+        );
+        Set<String> updatedMatchedItemsSet = new HashSet<>();
+        for (String item : matchedItemsSet) {
+            updatedMatchedItemsSet.add(replacementMap.getOrDefault(item, item));
+        }
+
         // 매칭된 아이템이 없으면 "No matched items"로 처리
-        List<String> matchedItems = new ArrayList<>(matchedItemsSet);
+        List<String> matchedItems = new ArrayList<>(updatedMatchedItemsSet);
         if (matchedItems.isEmpty()) {
             matchedItems.add("No matched items");
         }
@@ -143,7 +154,7 @@ public class YoloService {
         Set<String> matchedItemsSet = new HashSet<>();
         List<String> ITEMS_TO_CHECK = Arrays.asList(
                 "김치", "토마토", "방울토마토", "가지", "오이", "애호박", "팽이버섯", "새송이버섯",
-                "돼지고기", "닭고기", "소고기", "두부", "콩나물", "대파", "양파", "마늘", "시금치",
+                "두부", "콩나물", "대파", "양파", "마늘", "시금치",
                 "고추", "깻잎", "당근", "감자", "고구마", "계란", "무", "파프리카",
                 "배추", "열무", "고춧가루", "참기름", "들기름", "간장", "된장", "고추장",
                 "우유", "치즈", "버터", "요거트", "생수", "탄산수", "콜라", "사이다", "주스",
@@ -153,7 +164,7 @@ public class YoloService {
                 "미역", "다시마", "멸치", "김", "젓갈", "새우", "조개", "굴",
                 "감귤", "배", "포도", "바나나", "키위", "복숭아", "자두", "딸기", "체리", "블루베리",
                 "아몬드", "호두", "땅콩", "캐슈넛", "아보카도", "레몬", "라임",
-                "쌀", "생 돼지고기", "비엔나 소시지", "사과", "게 맛살", "어묵", "생 소고기", "생 닭고기"
+                "쌀", "생 돼지고기", "비엔나 소시지", "사과", "게 맛살", "어묵", "소", "닭", "돼지"
         );
 
         for (String item : items) {
